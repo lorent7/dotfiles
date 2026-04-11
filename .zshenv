2e3f169bf0caccfe8603783c0795a857bb1e2672
admin <lorentvrangalla@gmail.com>
2026-04-11 23:43:39 +0200

    feat: switch to chezmoi dotfiles manager
    Merged old history and added all files to chezmoi git
    
    chore: add template
    
    Update .config/helix/config.toml
    
    Update .config/.shell/.exports.sh
    
    Update .config/.shell/.exports.sh
    
    Remove .zshenv
    
    Remove .inputrc
    
    Update .config/.shell/.exports.sh
    
    Add .config/glow/glow.yml
    
    Add .config/readline/inputrc
    
    Add .config/docker-dash/config.toml
    
    Update .config/docker-dash/config.toml
    
    Update .config/tmux/tmux.conf

--- .zshenv
+++
@@ -1,2 +0,0 @@
-export ZDOTDIR="$HOME/.config/zsh"
-export ZCOMPDUMP="$XDG_CACHE_HOME/zsh/zcompdump-$HOST-$ZSH_VERSION"
--- private_dot_config/dot_shell/dot_exports.sh
+++ private_dot_config/dot_shell/dot_exports.sh
@@ -3,6 +3,8 @@ export EDITOR="helix"
 
 # These things don't need editing
 
+export ZDOTDIR="$HOME/.config/zsh"
+
 # XDG stuff
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
@@ -10,9 +12,6 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_BIN_HOME="$HOME/.local/bin"
 export XDG_LIB_HOME="$HOME/.local/lib"
 export XDG_CACHE_HOME="$HOME/.cache"
-
-# for PATH
-export PATH=$PATH:$XDG_BIN_HOME
 
 # python
 export PYTHONSTARTUP="$XDG_CONFIG_HOME/python/pythonrc"
@@ -22,7 +21,21 @@ export NODE_REPL_HISTORY="$XDG_STATE_HOME/node_history"
 
 export CARGO_HOME="$XDG_DATA_HOME/cargo"
 
+export GOPATH="$XDG_DATA_HOME"/go
+
 export WGETRC="$XDG_CONFIG_HOME/wgetrc"
+
+export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker
+
+export DOTNET_CLI_HOME="$XDG_DATA_HOME"/dotnet
+
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+
+export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
+
+# for PATH
+export PATH=$PATH:$XDG_BIN_HOME
+export PATH=$PATH:"$GOPATH"/bin
 
 # https://docs.npmjs.com/cli/v11/using-npm/config
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
@@ -30,17 +43,17 @@ export npm_config_cache="$XDG_CACHE_HOME/npm"
 
 # self explanatory but if its bash, give it bash history file location
 if [ -n "$BASH_VERSION" ]; then
-    export HISTFILE="$XDG_STATE_HOME/bash_history"
+  export HISTFILE="$XDG_STATE_HOME/bash_history"
 fi
 
 # same but zsh
-if  [ -n "$ZSH_VERSION" ]; then
-    export HISTFILE="$XDG_STATE_HOME/zsh_history"
-    # Path to your Oh My Zsh installation.
-    export ZSH="$XDG_DATA_HOME/oh-my-zsh"
-    export ZSH_CACHE_DIR="$XDG_CACHE_HOME/zsh"
-    mkdir -p "$ZSH_CACHE_DIR"
+if [ -n "$ZSH_VERSION" ]; then
+  export HISTFILE="$XDG_STATE_HOME/zsh_history"
+  # Path to your Oh My Zsh installation.
+  export ZSH="$XDG_DATA_HOME/oh-my-zsh"
+  export ZSH_CACHE_DIR="$XDG_CACHE_HOME/zsh"
+  mkdir -p "$ZSH_CACHE_DIR"
 
-    SAVEHIST=5000 # max saved cmds in ram
-    HISTSIZE=5000 # max saved cmds in history file
+  SAVEHIST=5000 # max saved cmds in ram
+  HISTSIZE=5000 # max saved cmds in history file
 fi
